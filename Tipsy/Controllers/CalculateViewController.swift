8967d81f3c259e18f7fa3cb26bbcf6d98121b9bf
Amit Singh <amitsighbxr@gmail.com>
2023-09-13 14:54:12 +0800

    second commit

--- Tipsy/Controllers/CalculateViewController.swift
+++ Tipsy/Controllers/CalculateViewController.swift
@@ -10,27 +10,57 @@ import UIKit
 
 class CalculateViewController: UIViewController {
 
-    @IBOutlet weak var ZeroPcButton: UIButton!
-    @IBOutlet weak var splirNumberLabel: UILabel!
     @IBOutlet weak var billTextField: UITextField!
-    @IBOutlet weak var tenPcButton: UIButton!
-    @IBOutlet weak var twentyPcButton: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        tip = buttonTitleAsANumber / 100
 
-    @IBAction func tipChnged(_ sender: UIButton) {
-       let buttonValue = sender.currentTitle!
-        if buttonValue  == "0%" {
-            ZeroPcButton.isSelected = true
-        }
-        //zeroPcButton.isSelected = true
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let bill = billTextField.text!
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
+        }
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goToResults" {
+            
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+        }
     }
 }
-
